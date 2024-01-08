595c590b4b26db852c268f190cf1dd19962f8288
Rémi COHEN SCALI <remi@cohenscali.net>
2024-01-08 15:32:37 +0100

    Update list.h
    Added comment for the file
    Added mutual exclusion pragma directive
    Added definition for typedef listchainee_ptr
    Added definition for listchainee_st

--- src/list.h
+++ src/list.h
@@ -1,4 +1,14 @@
 /*
- * list.h 
+ * list.h
  *
+ * Définition des structure de données pour la gestion de liste chainée
  */
+
+#pragma once
+
+typedef struct listchainee_st *listchainee_ptr;
+
+struct listechainee_st {
+  int N;
+  listchainee_ptr next;
+};
