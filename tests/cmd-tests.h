73838a132c0fbeec1ba18d8a0e96837457b02759
Rémi COHEN-SCALI <remi.cohen-scali@renault.com>
2024-01-15 00:43:45 +0100

    Updated cmd-tests.h
    Added decl for command line tests

--- tests/cmd-tests.h
+++ tests/cmd-tests.h
@@ -6,4 +6,16 @@
 
 #pragma once
 
-void test_command_basic_invocation(void);
+/* Declarations for command test functions */
+void test_command_basic_invocation_0(void);
+void test_command_basic_invocation_v(void);
+void test_command_basic_invocation_vh(void);
+void test_command_basic_invocation_verbose(void);
+void test_command_basic_invocation_verbose_help(void);
+void test_command_basic_invocation_A1d(void);
+void test_command_basic_invocation_P2d(void);
+void test_command_basic_invocation_append_3_display(void);
+void test_command_basic_invocation_prepend_4_display(void);
+void test_command_basic_invocation_A1P2d(void);
+void test_command_basic_invocation_append_1_prepend_2_display(void);
+
