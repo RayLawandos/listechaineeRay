90938c0c78cb43e71006b8951c3e44f02c60d050
Rémi COHEN-SCALI <remi.cohen-scali@renault.com>
2024-01-15 17:18:54 +0100

    Updated list-tests.h
    Added test_init/reinit

--- tests/list-tests.h
+++ tests/list-tests.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+void test_init_list(void);
+void test_reinit_list(void);
 void test_ix_in_list_with_list_null(void);
 void test_ix_in_list_with_list_with_ix(void);
 void test_ix_in_list_with_list_without_ix(void);
